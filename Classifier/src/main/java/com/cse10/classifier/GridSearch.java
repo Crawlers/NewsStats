d75875f48bfb26f5e44508a05dfda3f54cc2d1a5
=csgsajeewa <csgsajeewa@gmail.com>
2014-12-16 20:17:41 +0530

    Modify GridSearch Class

--- Classifier/src/main/java/com/cse10/classifier/GridSearch.java
+++ Classifier/src/main/java/com/cse10/classifier/GridSearch.java
@@ -21,11 +21,15 @@ public class GridSearch {
         }
     }
 
-    private static void gridSearch(LibSVM svm, Instances dataFiltered) throws Exception {
+    /**
+     * Perform grid search to find the best values for cost and gamma
+     * @param svm
+     * @param dataFiltered
+     */
+    public static void gridSearch(LibSVM svm, Instances dataFiltered){
         weka.classifiers.meta.GridSearch gs = new weka.classifiers.meta.GridSearch();
         gs.setClassifier(svm);
         gs.setFilter(new AllFilter());
-
 
         gs.setXProperty("classifier.cost");
         gs.setXMin(3);
@@ -40,35 +44,20 @@ public class GridSearch {
         gs.setYStep(0.5);
         gs.setYBase(2);
         gs.setYExpression("pow(BASE,I)");
-        //-y-property classifier.kernel.gamma -y-min -5.0 -y-max 2.0 -y-step 1.0 -y-base 10.0 -y-expression pow(BASE,I) -filter weka.filters.AllFilter -x-property classifier.nu -x-min 0.01 -x-max 1.0 -x-step 10.0 -x-base 10.0 -x-expression I -sample-size 100.0 -traversal COLUMN-WISE -log-file "C:\Program Files\Weka-3-6" -S 1 -W weka.classifiers.functions.LibSVM -- -S 2 -K 2 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.0010 -P 0.1
 
-        int EVALUATION_CC = 0;
-        int EVALUATION_RMSE = 1;
-        int EVALUATION_RRSE = 2;
-        int EVALUATION_MAE = 3;
-        int EVALUATION_RAE = 4;
-        int EVALUATION_COMBINED = 5;
-        int EVALUATION_ACC = 6;
-        int EVALUATION_KAPPA = 7;
-        Tag[] TAGS_EVALUATION = {
-                new Tag(EVALUATION_CC, "CC", "Correlation coefficient"),
-                new Tag(EVALUATION_RMSE, "RMSE", "Root mean squared error"),
-                new Tag(EVALUATION_RRSE, "RRSE", "Root relative squared error"),
-                new Tag(EVALUATION_MAE, "MAE", "Mean absolute error"),
-                new Tag(EVALUATION_RAE, "RAE", "Root absolute error"),
-                new Tag(EVALUATION_COMBINED, "COMB", "Combined = (1-abs(CC)) + RRSE + RAE"),
-                new Tag(EVALUATION_ACC, "ACC", "Accuracy"),
-                new Tag(EVALUATION_KAPPA, "KAP", "Kappa")
-        };
-        SelectedTag st = new SelectedTag(EVALUATION_ACC, TAGS_EVALUATION);
-        System.out.println(st.getTags());
+        int evaluationCriteriaIndex = 6;
+        SelectedTag st;
+        st = new SelectedTag(evaluationCriteriaIndex , weka.classifiers.meta.GridSearch.TAGS_EVALUATION);
         gs.setEvaluation(st);
-
+        gs.setGridIsExtendable(true);
         gs.setDebug(true);
-
-        gs.buildClassifier(dataFiltered);
-        System.out.println("Criteria " + gs.getEvaluation().getSelectedTag().getID());
-        System.out.println("&&&&&&&&&&&&" + gs.getValues());
+        try {
+            gs.buildClassifier(dataFiltered);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Criteria: " + gs.getEvaluation().getSelectedTag().getID());
+        System.out.println("Results: " + gs.getValues());
     }
 
 }
