9c47fd595230b1c0da9436435fc7524d80f8b02a
Tharindu Wijewardane <tharindu.moratuwa@gmail.com>
2014-12-19 19:22:26 +0530

    crime entities table added

---
+++ Extractor/src/main/java/com/cse10/extractor/ExtractorConstants.java
@@ -0,0 +1,39 @@
+package com.cse10.extractor;
+
+/**
+ * Created by TharinduWijewardane on 2014-12-19.
+ */
+public class ExtractorConstants {
+
+    public static final String CRIME_TYPE_MURDER = "murder";
+    public static final String CRIME_TYPE_RAPE = "rape";
+    public static final String CRIME_TYPE_STEAL = "steal";
+    public static final String CRIME_TYPE_TREASURE_HUNT = "treasure hunt";
+
+    public static final String DISTRICT_Ampara = "Ampara";
+    public static final String DISTRICT_Anuradhapura = "Anuradhapura";
+    public static final String DISTRICT_Badulla = "Badulla";
+    public static final String DISTRICT_Batticaloa = "Batticaloa";
+    public static final String DISTRICT_Colombo = "Colombo";
+    public static final String DISTRICT_Galle = "Galle";
+    public static final String DISTRICT_Gampaha = "Gampaha";
+    public static final String DISTRICT_Hambantota = "Hambantota";
+    public static final String DISTRICT_Jaffna = "Jaffna";
+    public static final String DISTRICT_Kalutara = "Kalutara";
+    public static final String DISTRICT_Kandy = "Kandy";
+    public static final String DISTRICT_Kegalle = "Kegalle";
+    public static final String DISTRICT_Kilinochchi = "Kilinochchi";
+    public static final String DISTRICT_Kurunegala = "Kurunegala";
+    public static final String DISTRICT_Mannar = "Mannar";
+    public static final String DISTRICT_Matale = "Matale";
+    public static final String DISTRICT_Matara = "Matara";
+    public static final String DISTRICT_Moneragala = "Moneragala";
+    public static final String DISTRICT_Mullaitivu = "Mullaitivu";
+    public static final String DISTRICT_Nuwara_Eliya = "Nuwara Eliya";
+    public static final String DISTRICT_Polonnaruwa = "Polonnaruwa";
+    public static final String DISTRICT_Puttalam = "Puttalam";
+    public static final String DISTRICT_Ratnapura = "Ratnapura";
+    public static final String DISTRICT_Trincomalee = "Trincomalee";
+    public static final String DISTRICT_Vavuniya = "Vavuniya";
+
+}
