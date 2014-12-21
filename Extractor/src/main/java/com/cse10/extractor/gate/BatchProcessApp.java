d3d633147df79a5bb43262d091c38585259e203d
Isuru Jayaweera <jayaweera63@gmail.com>
2014-12-22 00:19:58 +0530

    Setup to extract crime locations and districts for CeylonToday news articles and store extracted details in crime_entity_group table

--- Extractor/src/main/java/com/cse10/extractor/gate/BatchProcessApp.java
+++ Extractor/src/main/java/com/cse10/extractor/gate/BatchProcessApp.java
@@ -2,35 +2,40 @@ package com.cse10.extractor.gate;
 
 /**
  * Created with IntelliJ IDEA.
- * User: root
- * Date: 7/24/14
- * Time: 6:34 PM
- * To change this template use File | Settings | File Templates.
+ * User: Isuru Jayaweera
+ * Date: 10/24/14
+ * Extract entities from crime news articles and add those entities to a separate table.
  */
+
 import com.cse10.article.Article;
-import gate.Document;
-import gate.Corpus;
-import gate.CorpusController;
-import gate.AnnotationSet;
+import com.cse10.article.NewsFirstArticle;
+import com.cse10.database.DatabaseHandler;
+import com.cse10.entities.CrimeEntityGroup;
+import gate.*;
 import gate.Gate;
-import gate.Factory;
 import gate.annotation.AnnotationImpl;
 import gate.util.persistence.PersistenceManager;
 
-import java.util.*;
-
 import java.io.File;
-
-import com.cse10.database.DatabaseHandler;
+import java.util.*;
 
 public class BatchProcessApp {
 
-   public static void main(String[] args) throws Exception {
+    // Path to the saved application file.
+    private static File gappFile = new File("Extractor/src/main/resources/Location_v1.gapp");
 
-        File gateHome = new File("/home/isuru/Programs/GATE/gate-7.1-build4485-ALL");
-        gate.Gate.setGateHome(gateHome);
+    // List of annotation types to write out.  If null, write everything as GateXML.
+    private static List annotTypesToWrite = new ArrayList<>(Arrays.asList("CrimeLocation"));
 
-        // initialise GATE - this must be done before calling any GATE APIs
+    // fetch district name from google map api response
+    private static DistrictExtractor de = new DistrictExtractor();
+
+    // stores entities temporary till they are inserted to the table
+    private static ArrayList<CrimeEntityGroup> entityGroupsList = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+
+        // initialise GATE
         Gate.init();
 
         // load the saved application
@@ -40,87 +45,106 @@ public class BatchProcessApp {
         Corpus corpus = Factory.newCorpus("BatchProcessApp Corpus");
         application.setCorpus(corpus);
 
-       List<Article> articles = DatabaseHandler.fetchArticles(Article.class);
+        // fetches news articles from database
+        List<Article> articles = DatabaseHandler.fetchArticlesByIdRange(NewsFirstArticle.class, 0, 756);
 
         // process the files one by one
-        for(int i = 0; i < articles.size(); i++) {
+        for (int i = 0; i < articles.size(); i++) {
             Article currentArticle = articles.get(i);
 
-            // load the document (using the specified encoding if one was given)
-            Document doc = Factory.newDocument(currentArticle.getContent());
+            // load the document
+            Document doc = null;
+            try {
+                doc = Factory.newDocument(currentArticle.getContent());
+            } catch (OutOfMemoryError e) {
+                handleOutOfMemory(doc);
+                continue;
+            }
 
             // put the document in the corpus
             corpus.add(doc);
 
             // run the application
-            application.execute();
+            try {
+                application.execute();
+            } catch (OutOfMemoryError e) {
+                handleOutOfMemory(doc);
+                continue;
+            }
+
 
             // remove the document from the corpus again
             corpus.clear();
 
-            String docXMLString = null;
             Set annotationsToWrite = new HashSet();
-            // if we want to just write out specific annotation types, we must
-            // extract the annotations into a Set
-            if(annotTypesToWrite != null) {
-                // Create a temporary Set to hold the annotations we wish to write out
 
-                // we only extract annotations from the default (unnamed) AnnotationSet
-                // in this example
+            // extracting the annotations into a Set
+            if (annotTypesToWrite != null) {
+
+                // extracting annotations from the default AnnotationSet
                 AnnotationSet defaultAnnots = doc.getAnnotations();
                 Iterator annotTypesIt = annotTypesToWrite.iterator();
-                while(annotTypesIt.hasNext()) {
-                    // extract all the annotations of each requested type and add them to
+                while (annotTypesIt.hasNext()) {
+                    // extracting all the annotations of each requested type and add them to
                     // the temporary set
                     AnnotationSet annotsOfThisType =
-                            defaultAnnots.get((String)annotTypesIt.next());
-                    if(annotsOfThisType != null) {
+                            defaultAnnots.get((String) annotTypesIt.next());
+                    if (annotsOfThisType != null) {
                         annotationsToWrite.addAll(annotsOfThisType);
                     }
                 }
             }
 
+            System.out.println("Article : " + i + " -Begins Here-");
 
-            System.out.println("Article : "+i+" -Begins Here-");
-            // Release the document, as it is no longer needed
+            // Release the document
             Factory.deleteResource(doc);
 
+            // location related entities
+            String district = "NULL";
+            String location = "NULL";
+
+            // iterate through each annotation
             Iterator annotIt = annotationsToWrite.iterator();
-            while(annotIt.hasNext()) {
+            while (annotIt.hasNext()) {
                 // extract all the annotations of each requested type and add them to
                 // the temporary set
-                AnnotationImpl CurrentAnnot = (AnnotationImpl)annotIt.next();
-                String antText = gate.Utils.stringFor(doc,CurrentAnnot);
-                if(CurrentAnnot.getType().equalsIgnoreCase("Person")){
-                    System.out.println("Person : "+antText);
-                }   else if(CurrentAnnot.getType().equalsIgnoreCase("Organization")){
-                    System.out.println("Organization : "+antText);
-                }  else if(CurrentAnnot.getType().equalsIgnoreCase("Date")){
-                    System.out.println("Date : "+antText);
+                AnnotationImpl CurrentAnnot = (AnnotationImpl) annotIt.next();
+                String antText = gate.Utils.stringFor(doc, CurrentAnnot);
+
+                // check for crime location annotation
+                if (CurrentAnnot.getType().equalsIgnoreCase("CrimeLocation")) {
+                    System.out.println("CrimeLocation : " + antText);
+                    location = antText;
+
+                    // fetch district for the location using google map api
+                    district = de.getDistrict(location);
+                    if (!district.equalsIgnoreCase("NULL")) {
+                        CrimeEntityGroup entityGroupOfArticle = new CrimeEntityGroup();
+                        entityGroupOfArticle.setCrimeArticleId(currentArticle.getId());
+                        entityGroupOfArticle.setLocation(location);
+                        entityGroupOfArticle.setDistrict(district);
+                        entityGroupsList.add(entityGroupOfArticle);
+                    }
+                    System.out.println("District : " + district);
                 }
             }
-            System.out.println("Article : "+i+" -Ends Here-");
+            System.out.println("Article : " + i + " -Ends Here-");
+
 
             System.out.println("done");
         } // for each file
 
+        // if any entity has been extracted add them into database table
+        if (entityGroupsList.size() > 0) {
+            DatabaseHandler.insertCrimeEntityGroups(entityGroupsList);
+        }
+
         System.out.println("All done");
-    } // void main(String[] args)
+    }
 
-
-    /** Path to the saved application file. */
-    private static File gappFile = new File("Extractor/src/main/resources/PersonSearch.gapp");
-
-    /**
-     * List of annotation types to write out.  If null, write everything as
-     * GateXML.
-     */
-    private static List annotTypesToWrite = new ArrayList<String>(Arrays.asList("Person","Organization","Date"));
-
-    /**
-     * The character encoding to use when loading the docments.  If null, the
-     * platform default encoding is used.
-     */
-    private static String encoding = null;
-
+    private static void handleOutOfMemory(Document document){
+        Factory.deleteResource(document);
+        System.out.println("Memory Full - Leaving the Article");
+    }
 }
