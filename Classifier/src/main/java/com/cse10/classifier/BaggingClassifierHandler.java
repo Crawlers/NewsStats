2783aa2a1e561e2d8051dabfc184f3970684bc71
=csgsajeewa <csgsajeewa@gmail.com>
2014-12-21 11:12:39 +0530

    add bagging classifier

---
+++ Classifier/src/main/java/com/cse10/classifier/BaggingClassifierHandler.java
@@ -0,0 +1,78 @@
+package com.cse10.classifier;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.meta.Bagging;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import java.util.Random;
+
+/**
+ * wrapper class for bagging
+ * Created by chamath on 12/20/2014.
+ */
+public class BaggingClassifierHandler {
+    private Bagging bagging;
+
+    public BaggingClassifierHandler(Bagging bagging) {
+        this.bagging = bagging;
+    }
+
+
+    /**
+     *
+     * @param numOfIterations
+     * @param classifier
+     */
+    public void configure(int numOfIterations,Classifier classifier){
+        bagging.setNumIterations(numOfIterations);
+        bagging.setClassifier(classifier);
+    }
+
+    /**
+     *
+     * @param trainingDataFiltered
+     * @param numOfFolds
+     */
+    public void crossValidateClassifier(Instances trainingDataFiltered,int numOfFolds){
+
+        //perform cross validation
+        Evaluation evaluation = null;
+        try {
+            evaluation = new Evaluation(trainingDataFiltered);
+            evaluation.crossValidateModel(bagging, trainingDataFiltered, numOfFolds, new Random(1));
+            System.out.println(evaluation.toSummaryString());
+            System.out.println(evaluation.weightedAreaUnderROC());
+            double[][] confusionMatrix = evaluation.confusionMatrix();
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    System.out.print(confusionMatrix[i][j] + "  ");
+
+                }
+                System.out.println();
+            }
+            System.out.println("accuracy for crime class= " + (confusionMatrix[0][0] / (confusionMatrix[0][1] + confusionMatrix[0][0])) * 100 + "%");
+            System.out.println("accuracy for other class= " + (confusionMatrix[1][1] / (confusionMatrix[1][1] + confusionMatrix[1][0])) * 100 + "%");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * classify the article
+     * @param instance
+     * @return
+     */
+    public double classifyInstance(Instance instance){
+        double result=-1.0;
+        try {
+            result= bagging.classifyInstance(instance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
