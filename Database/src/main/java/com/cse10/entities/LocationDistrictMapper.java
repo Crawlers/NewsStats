9417221d7cb704cc9e0acdb05f6c5f651d7d4022
Isuru Jayaweera <jayaweera63@gmail.com>
2015-01-02 12:28:47 +0530

    File initialized

---
+++ Database/src/main/java/com/cse10/entities/LocationDistrictMapper.java
@@ -0,0 +1,43 @@
+package com.cse10.entities;
+
+import java.util.Date;
+
+/**
+ * Created by Isuru Jayaweera on 2014-12-28.
+ */
+public class LocationDistrictMapper implements java.io.Serializable {
+
+    private String location;
+    private String district;
+
+    public LocationDistrictMapper() {
+
+    }
+
+
+    public LocationDistrictMapper(String location, String district) {
+        this.location = location;
+        this.district = district;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDistrictForLocation(String location) {
+        return location;
+    }
+
+}
