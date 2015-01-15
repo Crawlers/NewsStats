205aaa20bf9f9f7235dba10d66fb4caedbcd171e
Tharindu Wijewardane <tharindu.moratuwa@gmail.com>
2015-01-15 07:45:48 +0530

    gui - classify task

---
+++ GUI/src/main/java/com/cse10/gui/task/CeylonTodayClassifyTask.java
@@ -0,0 +1,45 @@
+package com.cse10.gui.task;
+
+import javax.swing.*;
+import java.util.Random;
+
+/**
+ * Created by TharinduWijewardane on 2015-01-10.
+ */
+public class CeylonTodayClassifyTask extends SwingWorker<Void, Void> {
+    /*
+     * Main task. Executed in background thread.
+     */
+    boolean done = false;
+
+    @Override
+    public Void doInBackground() {
+        if (!done) {
+            System.out.println("in background");
+            Random random = new Random();
+            int progress = 0;
+            //Initialize progress property.
+            setProgress(0);
+            while (progress < 100) {
+                //Sleep for up to one second.
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ignore) {
+                }
+                //Make random progress.
+                progress += random.nextInt(10);
+                setProgress(Math.min(progress, 100));
+            }
+        }
+        return null;
+    }
+
+    /*
+     * Executed in event dispatching thread
+     */
+    @Override
+    public void done() {
+        System.out.println("done");
+        done = true;
+    }
+}
