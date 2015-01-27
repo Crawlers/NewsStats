8e6480cd8a01ab42a5a6a1a59b61f91e68b1fccc
Tharindu Wijewardane <tharindu.moratuwa@gmail.com>
2015-01-27 11:30:27 +0530

    added 2 methods

--- Database/src/main/java/com/cse10/database/DatabaseHandler.java
+++ Database/src/main/java/com/cse10/database/DatabaseHandler.java
@@ -487,7 +487,9 @@ public class DatabaseHandler {
 
         session.beginTransaction();
 
-        Integer count = (Integer) session.createCriteria(articleClass).add(Restrictions.eq("newspaper", newsPaper)).setProjection(Projections.max("newspaperId")).uniqueResult();
+        Integer count = (Integer) session.createCriteria(articleClass)
+                .add(Restrictions.eq("newspaper", newsPaper))
+                .setProjection(Projections.max("newspaperId")).uniqueResult();
 
         session.getTransaction().commit();
         session.close();
@@ -529,6 +531,52 @@ public class DatabaseHandler {
             return "";
         }
 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+
+    /**
+     * get earliest date with null label of the table containing articles of given type
+     *
+     * @param articleClass
+     * @return
+     */
+    public static String getEarliestDateWithNullLabel(Class articleClass) {
+
+        if (getRowCount(articleClass) == 0) { // if there are no articles
+            return "";
+        }
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        java.sql.Date latestDate = (java.sql.Date) session.createCriteria(articleClass)
+                .add(Restrictions.isNull("label"))
+                .setProjection(Projections.min("createdDate")).uniqueResult();
+        session.close();
+
+        java.util.Date date = new java.util.Date(latestDate.getTime()); //convert from sql date to util date
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+
+    /**
+     * get latest date with null label of the table containing articles of given type
+     *
+     * @param articleClass
+     * @return
+     */
+    public static String getLatestDateWithNullLabel(Class articleClass) {
+
+        if (getRowCount(articleClass) == 0) { // if there are no articles
+            return "";
+        }
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        java.sql.Date latestDate = (java.sql.Date) session.createCriteria(articleClass)
+                .add(Restrictions.isNull("label"))
+                .setProjection(Projections.max("createdDate")).uniqueResult();
+        session.close();
+
+        java.util.Date date = new java.util.Date(latestDate.getTime()); //convert from sql date to util date
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return sdf.format(date);
     }
