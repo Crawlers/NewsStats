fdfafe144fa3f03717e7ac7022afe84e22940954
Tharindu Wijewardane <tharindu.moratuwa@gmail.com>
2015-01-30 10:55:09 +0530

    util module added

---
+++ Util/src/main/java/com/cse10/util/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.cse10.util;
+
+/**
+ * Created by TharinduWijewardane on 2015-01-30.
+ */
+public class GlobalConstants {
+
+    final String PROXY_ADDRESS = "cache.mrt.ac.lk"; //"cache.mrt.ac.lk" //"";
+    final int PROXY_PORT = 3128; //3128 //0;
+
+}
