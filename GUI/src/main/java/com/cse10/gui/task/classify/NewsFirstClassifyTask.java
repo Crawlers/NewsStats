7694a2c6c0dd08791f4d0a0eb4abf849c98533b8
=csgsajeewa <csgsajeewa@gmail.com>
2015-02-02 13:36:36 +0530

    add news first classifier

--- GUI/src/main/java/com/cse10/gui/task/classify/NewsFirstClassifyTask.java
+++ GUI/src/main/java/com/cse10/gui/task/classify/NewsFirstClassifyTask.java
@@ -1,12 +1,17 @@
 package com.cse10.gui.task.classify;
 
+import com.cse10.classifier.DailyMirrorClassifierUIHandler;
+import com.cse10.classifier.NewsFirstClassifierUIHandler;
+
 import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Random;
 
 /**
  * Created by TharinduWijewardane on 2015-01-10.
  */
-public class NewsFirstClassifyTask extends ClassifyTask {
+public class NewsFirstClassifyTask extends ClassifyTask implements Observer{
 
     public NewsFirstClassifyTask() {
     }
@@ -21,21 +26,21 @@ public class NewsFirstClassifyTask extends ClassifyTask {
     @Override
     public Void doInBackground() {
         if (!done) {
-            System.out.println("in background");
-            Random random = new Random();
-            int progress = 0;
+            System.out.println("NewsFirst Classifer -> In Background");
+            Thread.currentThread().setName("NewsFirst Classifier Thread");
             //Initialize progress property.
             setProgress(0);
-            while (progress < 100) {
-                //Sleep for up to one second.
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ignore) {
-                }
-                //Make random progress.
-                progress += random.nextInt(10);
-                setProgress(Math.min(progress, 100));
-            }
+
+            //start classification process
+            classifierUIHandler = new NewsFirstClassifierUIHandler();
+            classifierUIHandler.getClassifierConfigurator().addObserver(this);
+            classifierUIHandler.setEndDate(endDate);
+            classifierUIHandler.setName("NewsFirst Classifier Thread");
+            classifierUIHandler.run();
+            classifierUIHandler.getClassifierConfigurator().deleteObserver(this);
+
+            System.out.println("NewsFirst Classifer -> Finished Task");
+
         }
         return null;
     }
@@ -45,7 +50,15 @@ public class NewsFirstClassifyTask extends ClassifyTask {
      */
     @Override
     public void done() {
-        System.out.println("done");
+        System.out.println("NewsFirst Classifer -> Done " +classifierUIHandler.isAlive());
         done = true;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        String message = (String) arg;
+        String[] m = message.split(" ");
+        if (m[0].equals("article_news_first"))
+            setProgress(Integer.parseInt(m[1]));
     }
 }
