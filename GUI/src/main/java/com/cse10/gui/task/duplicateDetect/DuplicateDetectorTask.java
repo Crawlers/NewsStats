d17ac8f47f6b03590d90bcab3e7a2a42fac1d6ab
=csgsajeewa <csgsajeewa@gmail.com>
2015-02-03 21:18:05 +0530

    update duplicate detector UI task

--- GUI/src/main/java/com/cse10/gui/task/duplicateDetect/DuplicateDetectorTask.java
+++ GUI/src/main/java/com/cse10/gui/task/duplicateDetect/DuplicateDetectorTask.java
@@ -1,5 +1,7 @@
 package com.cse10.gui.task.duplicateDetect;
 
+import com.cse10.duplicateDetector.DuplicateDetectorUIHandler;
+
 import javax.swing.*;
 import java.util.Observable;
 import java.util.Observer;
@@ -10,6 +12,14 @@ import java.util.Observer;
 public class DuplicateDetectorTask extends SwingWorker implements Observer {
 
     protected boolean done = false;
+    protected Thread duplicateDetectorUIHandlerThread;
+    protected DuplicateDetectorUIHandler duplicateDetectorUIHandler;
+
+    public DuplicateDetectorTask(){
+        this.duplicateDetectorUIHandler=new DuplicateDetectorUIHandler();
+        duplicateDetectorUIHandlerThread=new Thread(duplicateDetectorUIHandler);
+
+    }
 
     /*
      * Main task. Executed in background thread.
@@ -17,15 +27,22 @@ public class DuplicateDetectorTask extends SwingWorker implements Observer {
     @Override
     protected Object doInBackground() {
         if (!done) {
-            System.out.println("in background");
-
+            System.out.println("Duplicate Detector -> In Background");
+            duplicateDetectorUIHandler.addObserver(this);
+            duplicateDetectorUIHandlerThread.start();
+            try {
+                duplicateDetectorUIHandlerThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Duplicate Detector ->  Finished Task");
         }
 
         return null;
     }
 
     public void stop() {
-
+        duplicateDetectorUIHandlerThread.interrupt();
     }
 
     /*
@@ -33,12 +50,13 @@ public class DuplicateDetectorTask extends SwingWorker implements Observer {
      */
     @Override
     public void done() {
-        System.out.println("done");
+        System.out.println("Duplicate Detector -> Done");
         done = true;
     }
 
     @Override
     public void update(Observable o, Object arg) {
-
+        int progress=(Integer)arg;
+        setProgress(progress);
     }
 }
