52f2bbea91e70f4a3fa3b56308cb4801eb93a85f
=csgsajeewa <csgsajeewa@gmail.com>
2015-02-07 20:18:05 +0530

    complete unit testing

--- DuplicateDetector/src/test/java/com/cse10/duplicateDetector/SimHashCalculatorTest.java
+++ DuplicateDetector/src/test/java/com/cse10/duplicateDetector/SimHashCalculatorTest.java
@@ -25,9 +25,9 @@ public class SimHashCalculatorTest {
 
     @Test
     public void testSimhash64() throws Exception {
-        String testArticleContent1= "child sex crime 2012-11-08 Anuradhapura Anuradhapura Anuradhapura Anuradhapura";
+        String testArticleContent1= "child sex crime 2012 11 08 Anuradhapura Anuradhapura Anuradhapura Anuradhapura";
         String testArticleContent2="illegal trade 2012 12 26 Anuradhapura Anuradhapura Anuradhapura Anuradhapura";
-        long testValue1=213632246078292797L;
+        long testValue1=212506208464060957L;
         long testValue2=1348539346806932285L;
         TestCase.assertEquals(testValue1,simHashCalculator.getSimhash64Value(testArticleContent1));
         TestCase.assertEquals(testValue2,simHashCalculator.getSimhash64Value(testArticleContent2));
@@ -36,9 +36,9 @@ public class SimHashCalculatorTest {
 
     @Test
     public void testSimhash32() throws Exception {
-        String testArticleContent1= "child sex crime 2012-11-08 Anuradhapura Anuradhapura Anuradhapura Anuradhapura";
+        String testArticleContent1= "child sex crime 2012 11 08 Anuradhapura Anuradhapura Anuradhapura Anuradhapura";
         String testArticleContent2="illegal trade 2012 12 26 Anuradhapura Anuradhapura Anuradhapura Anuradhapura";
-        long testValue1=-2145704282;
+        long testValue1=270214823;
         long testValue2=-1877269851;
         TestCase.assertEquals(testValue1,simHashCalculator.getSimhash32Value(testArticleContent1));
         TestCase.assertEquals(testValue2,simHashCalculator.getSimhash32Value(testArticleContent2));
