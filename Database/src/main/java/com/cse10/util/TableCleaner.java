76963d6295e9f91f2fbf7ef5b5835f55f7fbe352
Tharindu Wijewardane <tharindu.moratuwa@gmail.com>
2015-02-07 12:19:26 +0530

    TableCleaner added. EmptyTables deleted

---
+++ Database/src/main/java/com/cse10/util/TableCleaner.java
@@ -0,0 +1,45 @@
+package com.cse10.util;
+
+import com.cse10.article.*;
+import com.cse10.database.DatabaseConstants;
+import com.cse10.database.DatabaseHandler;
+import com.cse10.entities.CrimeEntityGroup;
+
+/**
+ * Created by TharinduWijewardane on 2015-02-07.
+ */
+public class TableCleaner {
+
+    /**
+     * Undo the classification (so that it can be performed from the beginning again)
+     *
+     * @param deleteCrimeArticles : if the article_crime table should be emptied too
+     */
+    public static void undoClassifications(boolean deleteCrimeArticles) {
+
+        String tableCeylonToday = DatabaseConstants.classToTableName.get(CeylonTodayArticle.class);
+        String tableDailyMirror = DatabaseConstants.classToTableName.get(DailyMirrorArticle.class);
+        String tableNewsFirst = DatabaseConstants.classToTableName.get(NewsFirstArticle.class);
+        String tableTheIsland = DatabaseConstants.classToTableName.get(TheIslandArticle.class);
+
+        DatabaseHandler.executeUpdate("UPDATE " + tableCeylonToday + " SET label = NULL");
+        DatabaseHandler.executeUpdate("UPDATE " + tableDailyMirror + " SET label = NULL");
+        DatabaseHandler.executeUpdate("UPDATE " + tableNewsFirst + " SET label = NULL");
+        DatabaseHandler.executeUpdate("UPDATE " + tableTheIsland + " SET label = NULL");
+
+        if (deleteCrimeArticles) {
+            String tableCrimeArticles = DatabaseConstants.classToTableName.get(CrimeArticle.class);
+            DatabaseHandler.executeUpdate("DELETE FROM " + tableCrimeArticles);
+        }
+    }
+
+    /**
+     * Undo the duplicate detection (so that it can be performed from the beginning again)
+     */
+    public static void undoDuplicateDetection() {
+
+        String tableName = DatabaseConstants.classToTableName.get(CrimeEntityGroup.class);
+        DatabaseHandler.executeUpdate("UPDATE " + tableName + " SET label = NULL");
+    }
+
+}
